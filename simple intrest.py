51d5df1053116620e062e769db9d9f2d63251bb3
Pullasruthi14 <112850270+Pullasruthi14@users.noreply.github.com>
2022-09-05 14:50:19 +0530

    Add files via upload

---
+++ simple intrest.py
@@ -0,0 +1,5 @@
+principal=float(input('enter the principal amount'))
+time=float(input('enter the time period'))
+rate=float(input('enter the rate'))
+simple_intrest=(principal*time*rate)/100
+print("simple intrest:",simple_intrest)
