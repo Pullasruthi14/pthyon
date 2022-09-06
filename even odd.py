c42d57cd19261cf7192954f0ee91582219827e30
Pullasruthi14 <112850270+Pullasruthi14@users.noreply.github.com>
2022-09-06 18:14:57 +0530

    Add files via upload

---
+++ even odd.py
@@ -0,0 +1,9 @@
+def num(n):
+    if(n%2==0):
+        print("even number")
+    else:
+            print("odd number")
+    return num
+n=int(input("enter a number="))
+number = num(n)
+print(number)
